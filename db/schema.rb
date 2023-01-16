a584672304f1cd4e9357905f3ed4ba5830577594
shaik1729 <shaiktajuddinshavali@gmail.com>
2023-01-16 11:28:03 +0530

    roles model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_16_055301) do
+ActiveRecord::Schema.define(version: 2023_01_16_055620) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2023_01_16_055301) do
   create_table "colleges", force: :cascade do |t|
     t.string "name"
     t.string "short_form"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
     t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
