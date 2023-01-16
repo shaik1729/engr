183d3fd70a58e7fc59cd8cd21c2d49f68857649e
shaik1729 <shaiktajuddinshavali@gmail.com>
2023-01-16 11:32:31 +0530

    validation added for roles and college model

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,2 +1,16 @@
 class College < ApplicationRecord
+    before_save :upcase_fields
+
+    validates :name, presence: true, uniqueness: true
+    validates :short_form, presence: true, uniqueness: true
+    validates :code, presence: true, uniqueness: true
+
+    private
+
+    def upcase_fields
+        self.name = self.name.upcase
+        self.short_form = self.short_form.upcase
+        self.code = self.code.upcase
+    end
+
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,14 @@
 class Role < ApplicationRecord
+    before_save :upcase_fields
+
+    validates :name, presence: true, uniqueness: true
+    validates :code, presence: true, uniqueness: true
+
+    private
+
+    def upcase_fields
+        self.name = self.name.upcase
+        self.code = self.code.upcase
+    end
+
 end
