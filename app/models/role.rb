ab0898152f31d87cd307302d80dcc1f968ad0257
shaik1729 <shaiktajuddinshavali@gmail.com>
2023-01-19 22:48:58 +0530

    roles model added with helper methods

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,6 +6,18 @@ class Role < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :code, presence: true, uniqueness: true
 
+    def is_admin?
+        self.code == "ADMIN"
+    end
+
+    def is_faculty?
+        self.code == "FAC"
+    end
+
+    def is_student?
+        self.code == "STU"
+    end
+
     private
 
     def upcase_fields
