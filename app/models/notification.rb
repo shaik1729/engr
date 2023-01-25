a564486296c0e67eb8af19c41643c7c9271d29e4
shaik1729 <shaiktajuddinshavali@gmail.com>
2023-01-25 12:57:48 +0530

    file validation in notifications module implemented

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -9,4 +9,16 @@ class Notification < ApplicationRecord
     validates :title, presence: true
     validates :file, presence: true
 
+    validate :validate_document
+
+    private
+
+    def validate_document
+        if file.attached? && !file.content_type.in?(%w(application/zip application/pdf))
+          errors.add(:file, 'Must be a PDF or a ZIP file')
+        end
+        if file.attached? && file.blob.byte_size > 5.megabytes
+          errors.add(:file, 'Must be less than 5MB in size')
+        end
+    end
 end
